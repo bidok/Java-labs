dc117f91ca926e0b4d333d14e7afa27c195b9961
bidok <71400362+bidok@users.noreply.github.com>
2020-09-16 22:23:41 +0300

    Create lab_3.java

---
+++ lab_3.java
@@ -0,0 +1,29 @@
+
+import static java.lang.Math.*;
+
+public class lab_1_3 {
+    public static void main(String[] args) {
+        double totalArea =0;
+        double start = 0;
+        double finish = PI;
+        int numOfSquare = 10;
+        double y = 0.7;
+
+        double segment = finish - start;
+        double weidth = segment/numOfSquare;
+
+        for (int i = 1; i < numOfSquare; i++) {
+            double area = 0;
+            double height = sin(i*segment/numOfSquare);
+            if (height < y) {
+                area = height * weidth;
+                System.out.println("area of " + i +" square = " + area);
+            } else {
+                area = y * weidth;
+                System.out.println("area of " + i +" square = " + area);
+            }
+            totalArea += area;
+        }
+        System.out.println("your integral = " + totalArea);
+    }
+}
